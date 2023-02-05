89388e90db06c4fdbccd7c81c2bf43f75a405a5d
tulayevibrohim <tulayevibrohimjon@gmail.com>
2023-02-05 20:57:20 +0500

    hii

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a, b;
+
+        //declare variables
+        a = 1;
+        b = 2;
+
+        //formula
+        double result = (9 * a*a - (5 * b) + 2 + a - 7) * ((a + b - (4 * a * b)) / 2);
+
+        System.out.println("Result: " + result);
     }
 }
